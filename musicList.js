6ff92c5f88e8a087e9bda49063dda8261b2a70d4
ratneshchipre <ratneshchipre@gmail.com>
2025-01-02 15:41:43 +0530

    Adding files

---
+++ musicList.js
@@ -0,0 +1,50 @@
+export let musicList = [
+    {
+        name: "On The Floor",
+        artist: "Jennifer Lopez",
+        img: "img-1",
+        src: "music-1"
+    },
+    {
+        name: "Heat Waves",
+        artist: "Glass Animals",
+        img: "img-2",
+        src: "music-2"
+    },
+    {
+        name: "Tu Hai Kahan",
+        artist: "AUR",
+        img: "img-3",
+        src: "music-3"
+    },
+    {
+        name: "One Love",
+        artist: "Shubh",
+        img: "img-4",
+        src: "music-4"
+    },
+    {
+        name: "Blinding Lights",
+        artist: "The Weeknd",
+        img: "img-5",
+        src: "music-5"
+    },
+    {
+        name: "Infinity",
+        artist: "Jaymes Young",
+        img: "img-6",
+        src: "music-6"
+    },
+    {
+        name: "Hymn For The Weekend",
+        artist: "Coldplay",
+        img: "img-7",
+        src: "music-7"
+    },
+    {
+        name: "Kyun",
+        artist: "Pritam, Papon, Sunidhi Chauhan",
+        img: "img-8",
+        src: "music-8"
+    }
+]
---
+++ script.js
@@ -0,0 +1,117 @@
+import { musicList } from "./musicList.js";
+
+const wrapper = document.querySelector('.wrapper');
+const currentSongImg = document.querySelector('#song-img');
+const songName = document.querySelector('.song-name');
+const artistName = document.querySelector('.song-artist');
+const currentTimeDisplay = document.querySelector('.current-time');
+const totalDurationDisplay = document.querySelector('.total-time');
+const mainSong = document.querySelector('.main-song');
+const progressDiv = document.querySelector('.progress-div');
+const progressBar = document.querySelector('.progress-line');
+const playPause = document.querySelector('.play-pause');
+const playPauseBtn = document.querySelector('#play-pause-button');
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+
+let songIndex = 2;
+
+window.addEventListener('load', () => {
+    loadSong(songIndex);
+})
+
+function loadSong(indexNumb) {
+    songName.innerText = musicList[indexNumb - 1].name;
+    artistName.innerText = musicList[indexNumb - 1].artist;
+    currentSongImg.src = `/assets/images/${musicList[indexNumb - 1].img}.png`;
+    mainSong.src = `/assets/audio/${musicList[indexNumb - 1].src}.mp3`
+}
+
+playPause.addEventListener('click', () => {
+    const isSongPlay = wrapper.classList.contains('paused');
+    isSongPlay ? pauseSong() : playSong();
+});
+
+function pauseSong() {
+    wrapper.classList.remove('paused');
+    playPauseBtn.classList.remove("fa-pause");
+    playPauseBtn.classList.add("fa-play");
+    mainSong.pause();
+}
+
+function playSong() {
+    wrapper.classList.add('paused');
+    playPauseBtn.classList.remove("fa-play");
+    playPauseBtn.classList.add("fa-pause");
+    mainSong.play();
+}
+
+prevButton.addEventListener('click', () => {
+    playPrevSong();
+});
+
+function playPrevSong() {
+    songIndex--;
+    songIndex < 1 ? songIndex = musicList.length : songIndex = songIndex;
+    loadSong(songIndex);
+    playSong();
+    playPauseBtn.classList.remove("fa-play");
+    playPauseBtn.classList.add("fa-pause");
+    mainSong.play();
+}
+
+nextButton.addEventListener('click', () => {
+    playNextSong();
+});
+
+function playNextSong() {
+    songIndex++;
+    songIndex > musicList.length ? songIndex = 1 : songIndex = songIndex;
+    loadSong(songIndex);
+    playSong();
+    playPauseBtn.classList.remove("fa-play");
+    playPauseBtn.classList.add("fa-pause");
+    mainSong.play();
+}
+
+mainSong.addEventListener('timeupdate', (e) => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`
+
+    if(currentTime === duration) {
+        playPauseBtn.classList.remove("fa-pause");
+        playPauseBtn.classList.add("fa-play");
+    }
+
+    mainSong.addEventListener('loadeddata', () => {
+        let audioDuration = mainSong.duration;
+
+        let durationInMin = Math.floor(audioDuration / 60);
+        let durationInSeconds = Math.floor(audioDuration % 60);
+        if(durationInSeconds < 10) {
+            durationInSeconds = `0${durationInSeconds}`
+        }
+
+        totalDurationDisplay.innerText = `${durationInMin}:${durationInSeconds}`
+    })
+
+    let currentSongInMin = Math.floor(currentTime / 60);
+    let currentSongInSeconds = Math.floor(currentTime % 60);
+    if(currentSongInSeconds < 10) {
+        currentSongInSeconds = `0${currentSongInSeconds}`
+    }
+
+    currentTimeDisplay.innerText = `${currentSongInMin}:${currentSongInSeconds}`
+})
+
+progressDiv.addEventListener('click', (e) => {
+    let progressWidthVal = progressDiv.clientWidth;
+    let clickedOffSetX = e.offsetX;
+    let songDuration = mainSong.duration;
+
+    mainSong.currentTime = (clickedOffSetX / progressWidthVal) * songDuration;
+    playSong();
+})
